a42f475d0733db48146f643002437cb787a7c927
Bisratworku <101055036+Bisratworku@users.noreply.github.com>
2026-04-14 01:32:33 -0700

    removed the sphere geometry

--- main.js
+++ main.js
@@ -4,17 +4,12 @@ import {drawThreeGeo} from './geojson/threeGeoJSON.js';
 
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
-/*const geometry = new THREE.SphereGeometry(2.5);
-const line = new THREE.LineBasicMaterial({ color: "black", transparent: true, opacity: 1 });
-const edgs = new THREE.EdgesGeometry(geometry, 1);
-const wireframe = new THREE.LineSegments(edgs, line);
-scene.add(wireframe);*/
 camera.position.z = 5;
 fetch("geojson/countries.json")
 .then(response => response.json())
@@ -22,11 +17,23 @@ fetch("geojson/countries.json")
     const countries = drawThreeGeo({
         json: data,
         radius: 2.5,
+        color : new THREE.Color().setHSL(0.33, 0.8, 0.5)
     }); 
     scene.add(countries);
     countries.rotation.z = 1550;
 });
+fetch("geojson/rivers.geojson")
+.then(response => response.json())
+.then(data => {
+    const rivers = drawThreeGeo({
+        json: data,
+        radius: 2.5,
+        color : new THREE.Color().setHSL(0.6, 1.0, 0.5)
+    }); 
 
+    scene.add(rivers);
+    rivers.rotation.z = 1550;
+});
 
 
 
